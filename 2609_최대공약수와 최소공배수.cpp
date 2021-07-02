10366c114403a49fa78d682d7939468a5c8c608d
lyd0223 <lyd0223@gmail.com>
2021-07-02 21:14:22 +0900

    Create 2609_최대공약수와 최소공배수.cpp

---
+++ 2609_최대공약수와 최소공배수.cpp
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int a, b;
+    cin >> a >> b;
+    int num1 = a, num2 = b;
+    int c;
+    while (b != 0)
+    {
+        c = a % b;
+        a = b;
+        b = c;
+    }
+    int gcd = a;
+    int lcm = num1 * num2 / gcd;
+    cout << gcd << "\n" << lcm;
+    
+    
+    
+}
