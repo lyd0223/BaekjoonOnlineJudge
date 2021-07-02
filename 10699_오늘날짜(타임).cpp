61b7065c1598290bac95bda3425d5c4cfda178ea
lyd0223 <lyd0223@gmail.com>
2021-07-02 12:00:08 +0900

    Create 10699_오늘날짜(타임).cpp

---
+++ 10699_오늘날짜(타임).cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+int main()
+{   
+    time_t timeTemp = time(&timeTemp);
+    struct tm* timeinfo;
+    
+    timeinfo = gmtime(&timeTemp);
+    
+    
+    string year = to_string(timeinfo->tm_year + 1900);
+    string month = "";
+    if (timeinfo->tm_mon + 1 < 10)
+    {
+        month += "0";
+    }
+    month += to_string(timeinfo->tm_mon+1);
+    string day = "";
+    if (timeinfo->tm_mday < 10)
+    {
+        day += "0";
+    }
+    day += to_string(timeinfo->tm_mday);
+    //cout << asctime(timeinfo);
+    cout << year << "-" << month << "-" << day;
+}
