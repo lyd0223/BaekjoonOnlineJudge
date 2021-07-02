f3e7c268528a9ae43434364c8f60ea9dfaefaa91
lyd0223 <lyd0223@gmail.com>
2021-07-02 21:14:12 +0900

    Create 1158_요세푸스문제(queue).cpp

---
+++ 1158_요세푸스문제(queue).cpp
@@ -0,0 +1,49 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+//queue를 이용해서 구현한다.
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int N , K;
+    cin >> N >> K;
+    
+    queue<int> q;
+    for (int i = 1; i <= N; i++)
+    {
+        q.push(i);
+    }
+    int ct = 1;
+    cout << "<";
+    while (!q.empty())
+    {
+        if (ct > K)
+            ct -= K;
+        if (ct == K)
+        {
+            if (q.size() == 1)
+                cout << q.front();
+            else
+                cout << q.front() << ", ";
+            
+            q.pop();
+           
+            ct++;
+        }
+        else
+        {
+            q.push(q.front());
+            q.pop();
+            ct++;
+        }
+    }
+    cout << ">";
+    
+    
+}
