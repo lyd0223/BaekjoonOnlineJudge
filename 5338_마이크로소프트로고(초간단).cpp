9417904b1818bab8d4b5a0bfcb84be063fb11858
lyd0223 <lyd0223@gmail.com>
2021-07-02 21:14:26 +0900

    Create 5338_마이크로소프트로고(초간단).cpp

---
+++ 5338_마이크로소프트로고(초간단).cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    cout << "       _.-;;-._\n";
+    cout << "'-..-'|   ||   |\n";
+    cout << "'-..-'|_.-;;-._|\n";
+    cout << "'-..-'|   ||   |\n";
+    cout << "'-..-'|_.-''-._|\n";
+}
