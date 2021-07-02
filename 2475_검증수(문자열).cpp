3c148fc1693571b9e4e78d84f1912f6f01952a76
lyd0223 <lyd0223@gmail.com>
2021-07-02 12:00:17 +0900

    Create 2475_검증수(문자열).cpp

---
+++ 2475_검증수(문자열).cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{   
+    int num =0;
+    for (int i = 0; i < 5; i++)
+    {
+        int temp;
+        cin >> temp;
+        
+        num += (temp * temp);
+    }
+    
+    string str = to_string(num);
+    
+    cout << (str[str.size() - 1]);
+         
+}
