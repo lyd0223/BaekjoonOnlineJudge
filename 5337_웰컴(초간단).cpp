ac6252ea740af1c8fbae90e2f9ec9e92adb67233
lyd0223 <lyd0223@gmail.com>
2021-07-02 21:14:24 +0900

    Create 5337_웰컴(초간단).cpp

---
+++ 5337_웰컴(초간단).cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    cout << ".  .   .\n";
+    cout << "|  | _ | _. _ ._ _  _\n";
+    cout << "|/\\|(/.|(_.(_)[ | )(/.\n";
+}
