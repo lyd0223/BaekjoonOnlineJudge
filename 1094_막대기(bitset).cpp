87a4fc51e1f8329f2ed48015459fd8b00fbd58c3
lyd0223 <lyd0223@gmail.com>
2021-07-02 21:14:10 +0900

    Create 1094_막대기(bitset).cpp

---
+++ 1094_막대기(bitset).cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int x;
+    cin >> x;
+    
+    bitset<8> bit(x);
+    
+    cout << bit.count();
+}
