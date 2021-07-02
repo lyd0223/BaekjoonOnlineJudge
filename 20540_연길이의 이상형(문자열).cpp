9851e50fb1ea880f80ab508927a4eb7edb74ffec
lyd0223 <lyd0223@gmail.com>
2021-07-02 12:00:13 +0900

    Create 20540_연길이의 이상형(문자열).cpp

---
+++ 20540_연길이의 이상형(문자열).cpp
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+	char myMBTI[5];
+	scanf("%s", myMBTI);
+	
+	string herMBTI ="";
+	
+	if (myMBTI[0] == 'E')
+		herMBTI += "I";
+	else
+		herMBTI += "E";
+
+	if (myMBTI[1] == 'S')
+		herMBTI += "N";
+	else
+		herMBTI += "S";
+
+	if (myMBTI[2] == 'T')
+		herMBTI += "F";
+	else
+		herMBTI += "T";
+
+	if (myMBTI[3] == 'J')
+		herMBTI += "P";
+	else
+		herMBTI += "J";
+
+	printf("%s", herMBTI.c_str());
+}
