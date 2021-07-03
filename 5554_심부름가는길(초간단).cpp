b92f673e94b039cce94d548a3bc608860727c47b
lyd0223 <lyd0223@gmail.com>
2021-07-03 20:50:08 +0900

    Create 5554_심부름가는길(초간단).cpp

---
+++ 5554_심부름가는길(초간단).cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    int time = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        int n;
+        cin >> n;
+        time += n;
+    }
+
+    int minutes = time / 60;
+    int seconds = time % 60;
+    cout << minutes << "\n" << seconds;
+}
+
