88dc2112b2403c571740f2df5a602d694937d7b6
lyd0223 <lyd0223@gmail.com>
2021-07-03 20:50:10 +0900

    Create 6749_Next in Line(초간단).cpp

---
+++ 6749_Next in Line(초간단).cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    int three, two;
+    
+    cin >> three >> two;
+    
+    int one = two + two - three;
+
+    cout << one;
+}
+
