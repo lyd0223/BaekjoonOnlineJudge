0ab50f5650f1bac47dc748fdae1f20d6ba820480
lyd0223 <lyd0223@gmail.com>
2021-07-03 20:50:04 +0900

    Create 5339_콜센터(출력).cpp

---
+++ 5339_콜센터(출력).cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    cout << "     /~\\\n";
+    cout << "    ( oo|\n";
+    cout << "    _\\=/_\n";
+    cout << "   /  _  \\\n";
+    cout << "  //|/.\\|\\\\\n";
+    cout << " ||  \\ /  ||\n";
+    cout << "============\n";
+    cout << "|          |\n";
+    cout << "|          |\n";
+    cout << "|          |\n";
+}
+
