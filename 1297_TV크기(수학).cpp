49c1792a8b71e5e0081982bb26fd729f301cad76
lyd0223 <lyd0223@gmail.com>
2021-07-03 20:49:51 +0900

    Create 1297_TV크기(수학).cpp

---
+++ 1297_TV크기(수학).cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    double c, w, h;
+    
+    cin >> c >> w >> h;
+    
+    int width, height;
+    
+	width = sqrt(pow(c, 2) * pow(w, 2) / (pow(w, 2) + pow(h, 2)));
+    height = sqrt(pow(c, 2) * pow(h, 2) / (pow(w, 2) + pow(h, 2)));
+    
+    cout << width << " " << height;
+}
+
