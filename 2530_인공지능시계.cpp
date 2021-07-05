b7ab2cea8858efc076ec19722357074ecdcb913f
lyd0223 <lyd0223@gmail.com>
2021-07-05 23:40:30 +0900

    Create 2530_인공지능시계.cpp

---
+++ 2530_인공지능시계.cpp
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    int h, m, s;
+    cin >> h >> m >> s;
+    int time;
+    cin >> time;
+    s += time;
+    while (s >= 60)
+    {
+        m++;
+        s -= 60;
+    }
+    while (m >= 60)
+    {
+        h++;
+        m -= 60;
+    }
+    while (h >= 24)
+    {
+        h -= 24;
+    }
+
+    cout << h << " " << m << " " << s;
+
+}
