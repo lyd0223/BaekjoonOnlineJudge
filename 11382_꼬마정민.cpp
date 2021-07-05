74cc825374060c2859875ce2f2743a1663a61f34
lyd0223 <lyd0223@gmail.com>
2021-07-05 18:10:19 +0900

    Create 11382_꼬마정민.cpp

---
+++ 11382_꼬마정민.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    long long a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c;
+}
