010b36e9607d17c13ac8e68631aaf63adb5128bf
lyd0223 <lyd0223@gmail.com>
2021-07-13 16:08:58 +0900

    Create 2164_카드2(queue).cpp

---
+++ 2164_카드2(queue).cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    int n;
+    cin >> n;
+    
+    queue<int> q;
+    for(int i = 1; i<= n; i++)
+    {
+        q.push(i);
+    }
+    while (q.size() > 1)
+    {
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+    cout << q.front();
+}
