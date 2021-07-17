54e471e79f0b08f2834b74ef32d330fcc679c8b6
lyd0223 <lyd0223@gmail.com>
2021-07-17 22:52:27 +0900

    Create 14645_와이버스부릉부릉.cpp

---
+++ 14645_와이버스부릉부릉.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    
+    int n, k;
+    cin >> n >> k;
+    
+    for (int i = 0; i < n; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+    }
+    cout << "ºñ¿ÍÀÌ";
+}
